7c180e37b3479bd2818ed3ad1ea8ac15003125b4
Siasa <84365286+siasa18037@users.noreply.github.com>
2024-11-10 12:48:07 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,12 +17,23 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// app.use(cors({
+//     origin: process.env.REACT_APP_FRONTEND_URL,
+//     // origin: "*",
+//     credentials: true
+// }));
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 app.use(cors({
-    origin: process.env.REACT_APP_FRONTEND_URL,
-    // origin: "*",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
 }));
-
 
 // Define routes
 app.use('/data',dataRoutes);
