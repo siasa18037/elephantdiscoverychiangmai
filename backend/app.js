1363cd4ce223b44c649a0e650f5a32ea9d96f7c6
Siasa <84365286+siasa18037@users.noreply.github.com>
2024-11-10 13:13:56 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -28,8 +28,6 @@ app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
         }
     },
     credentials: true
